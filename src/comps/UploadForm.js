c7b4ac140d03dd1b558fda646f50bfe702f63615
petkopetkov987 <61232356+petkopetkov987@users.noreply.github.com>
2020-08-19 16:27:43 +0300

    configure file form

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const UploadForm = () => {
+    const [file, setFile] = useState(null);
+    const [error, setError] = useState(null);
+
+    const types = ['image/png', 'image/jpeg'];
+
+    const changeHandler = (e) => {
+        let selected = e.target.files[0];
+        
+        if (selected && types.includes(selected)) {
+            setFile(selected);
+        } else {
+            setFile(null);
+            setError('The file must be png or jpeg!');
+        }
+    }
+
     return (
         <form>
-            <input type="file" />
+            <input type="file" onChange={changeHandler} />
         </form>
     )
 }
