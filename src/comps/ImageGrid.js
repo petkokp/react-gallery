dec598241307014e63540304b1a45aa61e0f60de
petkopetkov987 <61232356+petkopetkov987@users.noreply.github.com>
2020-08-22 17:22:50 +0300

    display images

--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -6,7 +6,11 @@ const ImageGrid = () => {
     
     return (
         <div className="img-grid">
-            images
+            { docs && docs.map(doc => (
+                <div className="img-wrap" key={doc.id}>
+                    <img src={doc.url} alt="The pic is uploaded" />
+                </div>
+            ))}
         </div>
     )
 }
