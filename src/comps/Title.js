ad165513647fc8c929f25e38a9136f30b5cebd98
petkopetkov987 <61232356+petkopetkov987@users.noreply.github.com>
2020-09-12 17:45:56 +0300

    make the title bold

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Title = () => {
   return (
     <div className="title">
-      <h1>React Gallery</h1>
+      <h1><b>React Gallery</b></h1>
       <h2>Your Photos</h2>
     </div>
   )
